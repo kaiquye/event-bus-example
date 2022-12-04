00d87d6280f6168d10848969338c42320a137766
kaiquye <kaiquemendesilva82@gmail.com>
2022-12-03 21:19:38 -0300

    :bug: rename event

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,22 @@ import { EventBusConnection } from './events';
 
 const eventBus = EventBusConnection.getInstance();
 
-eventBus.register('teste', new CreateUserEvent());
-eventBus.register('teste', new NotifyKafkaEvent());
-eventBus.register('teste', new SendEmailEvent());
+eventBus.register('event1', new CreateUserEvent());
+eventBus.register('event1', new NotifyKafkaEvent());
+eventBus.register('event1', new SendEmailEvent());
 
-eventBus.notify('teste', {
+eventBus.notify('event1', {
   eventData: 232,
   date: new Date(),
 });
 
 const eventBus2 = EventBusConnection.getInstance();
 
-eventBus2.register('teste', new CreateUserEvent());
-eventBus2.register('teste', new NotifyKafkaEvent());
-eventBus2.register('teste', new SendEmailEvent());
+eventBus2.register('event2', new CreateUserEvent());
+eventBus2.register('event2', new NotifyKafkaEvent());
+eventBus2.register('event2', new SendEmailEvent());
 
-eventBus2.notify('teste', {
+eventBus2.notify('event2', {
   eventData: 232,
   date: new Date(),
 });
